8f7254ff813f1c44056fb4282b4da7c0c58846b6
p1nta <crispworm@gmail.com>
2022-05-12 19:28:34 +0300

    chore: upd

--- src/catch-me/index.js
+++ src/catch-me/index.js
@@ -153,7 +153,7 @@ class UI {
         div.appendChild(front);
         div.appendChild(back);
         if (getImageFunction) {
-          getImageFunction(i, j, square, back);
+          getImageFunction(i, j, square - 1, back);
         }
         this.parentEl.appendChild(div);
       }
