ffab0936956b839c6ec87c5031e2a5d4ae576c67
p1nta <crispworm@gmail.com>
2022-05-13 14:17:42 +0300

    chore

--- src/catch-me/index.js
+++ src/catch-me/index.js
@@ -243,10 +243,10 @@ class Picture {
             };
 
             const { offsetHeight, offsetWidth } = document.body;
-            let k = Math.max(this.size.height / offsetHeight, this.size.width / offsetWidth);
+            let k = Math.max(this.size.height / (offsetHeight - 50), this.size.width / (offsetWidth - 50));
 
             if (k < 1) {
-              let k = Math.min(this.size.height / offsetHeight, this.size.width / offsetWidth);
+              let k = Math.min(this.size.height / (offsetHeight - 50), this.size.width / (offsetWidth - 50));
             }
 
             document.body.style.setProperty('--width', `${this.size.width / k}px`);
