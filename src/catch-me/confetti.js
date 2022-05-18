c1477f036b0fadd49aebad1433da91a09041bb8c
p1nta <crispworm@gmail.com>
2022-05-19 00:29:44 +0300

    chore: try to fix cors

--- src/catch-me/confetti.js
+++ src/catch-me/confetti.js
@@ -1,5 +1,5 @@
-var maxParticleCount = 350; //set max confetti count
-var particleSpeed = 5; //set the particle animation speed
+var maxParticleCount = 4000; //set max confetti count
+var particleSpeed = 2; //set the particle animation speed
 var startConfetti; //call to start confetti animation
 var stopConfetti; //call to stop adding confetti
 var toggleConfetti; //call to start or stop the confetti animation depending on whether it's already running
