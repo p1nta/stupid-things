171af627d89f5716897f3e4732716ea156f1189d
Ihor Pchelnik <30403859+p1nta@users.noreply.github.com>
2023-08-01 12:03:00 +0300

    Chore: Cleanup dead code. Add simple dark mode. Add makefile (#6)
    * chore: cleanup. Add simple darkmode
    
    * chore: udpate white color

--- src/hello-sergey/index.js
+++ src/hello-sergey/index.js
@@ -20,7 +20,7 @@ class Steps {
   agree() {
     if (this._step === 1) {
       this._step = 3;
-    } else if (this._step === 2){
+    } else if (this._step === 2) {
       this._step = 5;
     } else {
       this._step += 1;
@@ -40,16 +40,18 @@ class Steps {
   }
 }
 
-const body = document.body;
+window.onload = () => {
+  const body = document.body;
 
-window.stepsController = new Steps((step) => {
-  if (step === 5) {
-    const videoElement = document.getElementsByTagName('video');
+  window.stepsController = new Steps((step) => {
+    if (step === 5) {
+      const videoElement = document.getElementsByTagName('video');
 
-    if (videoElement && videoElement[0]) {
-      videoElement[0].play();
+      if (videoElement && videoElement[0]) {
+        videoElement[0].play();
+      }
     }
-  }
 
-  body.setAttribute('data-step', step);
-});
+    body.setAttribute('data-step', step);
+  });
+}
